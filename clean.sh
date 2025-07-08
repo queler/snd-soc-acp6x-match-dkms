1d805c9c05d05b4ccae8b4d46c383e3d12789825
Adam Queler <queler+k@gmail.com>
2025-07-08 14:31:20 -0400

    helper scripts. i think problem is clean

--- clean.sh
+++ clean.sh
@@ -10,5 +10,5 @@ odpath="$(realpath 'sound/soc/amd/yc/')"
 ksrc=/usr/src/linux-headers-$kver
 dvar kernelver kver kengenver modpath ksrc
 echo "cleaning  make -C \"$ksrc\" M=\"$modpath\" clean"
-make -C "$ksrc" M="$modpath" clean
+make -C "$ksrc" M="$modpath" clean &>clean.log
 
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo dkms remove -m snd-acp6x-fb1xxx -v 2.0 --all
+sudo rm -rv /usr/src/snd-acp6x-fb1xxx-2.0
