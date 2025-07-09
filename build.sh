116f1e135d03675464867f1af0eea42987c7a83d
Adam Queler <queler+k@gmail.com>
2025-07-09 12:05:14 -0400

    added makefile and centraluzed variables

--- build.sh
+++ build.sh
@@ -1,22 +1,9 @@
 #!/usr/bin/env bash
-
-function dvar() { for i in $@; do echo "${i}=${!i}";done;}
-echo "kernelver=$kernelver"
-echo "uname -r=$(uname -r)"
-kver="${kernelver:-$(uname -r)}"
-echo "using $kver"
-kgenver="${kver%%-*}"
-patch_file="acp6x-mach-fb1xxx.patch"
-tarball=$(find "/usr/src/" -maxdepth 1 -iname "linux-source-${kgenver}.tar.*"|head -n1)
-dvar kernelver kver kgenver patch_file tarball
+. buildenv
 echo "unpacking"
-tar xvf "$tarball" linux-source-6.8.0/sound/soc/amd/yc/ --strip-components=1
+tar xvf "$tarball" linux-source-${kgenver}/${modpath}/${modsrc} --strip-components=1
 echo 'patching'
 patch  --verbose -p1   < acp6x-mach-fb1xxx.patch
-
-modpath="$(realpath 'sound/soc/amd/yc/')"
-ksrc=/usr/src/linux-headers-$kver
-dvar modpath ksrc
 echo "making make -C \"$ksrc\" M=\"$modpath\""
-make -C "$ksrc" M="$modpath"
+make -C "$ksrc" M="$modpath" modules
 
--- clean.sh
+++ clean.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-function dvar() { for i in $@; do echo "${i}=${!i}";done;}
-echo "kernelver=$kernelver"
-echo "uname -r=$(uname -r)"
-kver="${kernelver:-$(uname -r)}"
-echo "using $kver"
-kgenver="${kver%%-*}"
+. buildenv
 if [[ -d "$modpath" ]]; then
-	modpath="$(realpath 'sound/soc/amd/yc/')"
+	realmodpath="$(realpath 'sound/soc/amd/yc/')"
 	ksrc=/usr/src/linux-headers-$kver
-	dvar kernelver kver kengenver modpath ksrc
+	dvar kernelver kver kengenver realmodpath ksrc
 	echo "cleaning  make -C \"$ksrc\" M=\"$modpath\" clean"
 	make -C "$ksrc" M="$modpath" clean 2>&1 |tee clean.log
 else
