48bb3996b78014f7da19907a2f54e90f41c86e85
Adam Queler <queler+k@gmail.com>
2025-07-09 18:55:25 -0400

    building whole  module

--- Makefile
+++
@@ -1,3 +0,0 @@
-# tell kbuild “I’m building one module, from this .c file”
-obj-m += snd-soc-acp6x-mach.o
-snd-soc-acp6x-mach-objs := sound/soc/amd/yc/acp6x-mach.o
--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ if [[ -f "fake.tar" ]]  then
 	echoing 'unpacking fake.tar for faster testing'
 	tar xvf "fake.tar" linux-source-${kgenver}/${modpath}/${modsrc} --strip-components=1
 else
-	tar xvf "$tarball" linux-source-${kgenver}/${modpath}/${modsrc} --strip-components=1
+	tar xvf "$tarball" linux-source-${kgenver}/${modpath}/ --strip-components=1
 fi
 echo 'patching'
 patch  --verbose -p1   < acp6x-mach-fb1xxx.patch
 
-make -C "$ksrc" M="$PWD" modules
+make -C "$ksrc" M="$modpath" modules
 
