ecd863e1a1aa29cade6f7b9d9c842fc51f4acc82
Adam <queler@gmail.com>
2025-07-13 22:32:27 -0400

    better all around

--- build.sh
+++ build.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 . dkms.conf
-echo "unpacking"
-#if [[ -f "../$(basename ${tarball%.*})" ]] ; then
-#	echo 'unpacking cached tar for faster testing'
-#	tar xvf "../$(basename ${tarball%.*})" linux-source-${kgenver}/${modpath}/$modsrc --strip-components=1
-#else
-	tar xvf "$tarball" linux-source-${kgenver}/${modpath}/$modsrc  --strip-components=1
-#fi
+bzopt=()
+case "$tarball" in
+    *.tar.bz2|*.tbz2)
+        bzcmd=$(command -v lbzip2 || command -v pbzip2 || command -v bzip2)
+        if [[ -n "$bzcmd" ]]; then
+            bzopt=(--use-compress-program="$bzcmd")
+        else
+            echo "No bzip2-compatible decompressor found." >&2
+            exit 1
+        fi
+        ;;
+esac
+dvar bzopt[@] bzcmd[@]
+echo "Unpacking..."
+tar "${bzopt[@]}" -xvf "$tarball" "linux-source-${kgenver}/${modpath}/" --strip-components=1
 echo 'patching'
 patch  --verbose -p1   < acp6x-mach-fb1xxx.patch
-
-make -C "$ksrc" M="$PWD/$modpath/$modsrc" modules
+echo "MODULE_VERSION(\"${PACKAGE_VERSION}-dkms$(git rev-parse --short HEAD||date "+%s")\");" >> sound/soc/amd/yc/acp6x-mach.c
+make -C "$ksrc" M="$PWD/$modpath/" $BUILT_MODULE_NAME.ko
